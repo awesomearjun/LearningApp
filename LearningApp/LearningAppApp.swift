ff727df892235d1231ad7acfcc38a826a5c684e0
awesomearjun <iamawesomearjun@gmail.com>
2021-05-30 20:52:11 -0400

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by A P on 2021-05-30.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
